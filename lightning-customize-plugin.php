fa9cb257908288a985c5b4b6b75ba86a5d808fad
kurudrive <kurudrive@gmail.com>
2020-04-11 15:06:48 +0900

    スマホヘッダーサンプル追加

--- lightning-customize-plugin.php
+++ lightning-customize-plugin.php
@@ -2,27 +2,19 @@
 /**
  * Plugin Name:     Lightning Customize Plugin
  * Plugin URI:      https://github.com/vektor-inc/lightning-customize-plugin
- * Description:     
- * Author:          
- * Author URI:      
+ * Description:
+ * Author:
+ * Author URI:
  * Text Domain:     lightning-customize-plugin
  * Domain Path:     /languages
  * Version:         0.1.0
- * License: 		GNU General Public License v2 or later
- * License URI: 	http://www.gnu.org/licenses/gpl-2.0.html
+ * License:         GNU General Public License v2 or later
+ * License URI:     http://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package         Lightning_Customize_Plugin
  */
 
 // Your code starts here.
 
-/*
-  Change Author Archive title
-/*-------------------------------------------*/
-function my_lightning_get_the_archive_title( $title ){
-	if ( is_author( )){
-		$title = '<span>' . esc_html( get_the_author() ). '</span>';
-	}
-	return $title;
-}
-add_filter( 'lightning_get_the_archive_title', 'my_lightning_get_the_archive_title' );
+
+
--- readme.md
+++ readme.md
@@ -13,3 +13,19 @@ function my_lightning_get_the_archive_title( $title ){
 }
 add_filter( 'lightning_get_the_archive_title', 'my_lightning_get_the_archive_title' );
 ```
+
+---
+
+### スマホでヘッダーに電話番号と問い合わせボタンを配置する
+
+add_action(
+	'lightning_header_logo_after',
+	function() {
+		?>
+		<div class="text-center font-weight-bold mt-1 mb-2 d-lg-none">
+			<div><a href="tel:000-000-0000" style="font-size:1.4rem;">000-000-0000</a></div>
+			<a href="/contact/" class="btn btn-primary btn-block">お問い合わせ</a>
+		</div>
+		<?php
+	}
+);
